c2aaadb1561197a21ace4e4e2b42b5f9b2190948
Muhammad Anas Sohail <anassohail34343@gmail.com>
2023-08-19 02:14:13 +0500

    buttons
    Delete and Edit buttons added in blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -15,6 +15,10 @@ const Blog = () => {
                     </div>
                 </div>
                 <p className={styles.blogdescription}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <div className={styles.btndiv}>
+                    <button type="button" className={`${styles.btn} btn  mt-4`}>Delete</button>
+                    <button type="button" className={`${styles.btn} btn  mt-4`}>Edit</button>
+                </div>
             </div>
         </div>
     )
